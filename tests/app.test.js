047bc7af497c30ef9ad2ca2a3885c4d8577bf541
82517 <2022itdream@gmail.com>
2026-09-26 14:27:37 -0400

    test: add jest configuration and initial unit test

---
+++ tests/app.test.js
@@ -0,0 +1,9 @@
+/**
+ * Initial test suite for Project Father
+ */
+
+describe("Project Father Initial Test Suite", () => {
+  test("should pass basic sanity check", () => {
+    expect(true).toBe(true);
+  });
+});
